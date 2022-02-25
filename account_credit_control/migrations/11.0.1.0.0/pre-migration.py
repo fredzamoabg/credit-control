d22b340455844a541263149e05b5536d6bd276f0
Alfredo Zamora <alfredo.zamora@agilebg.com>
2022-02-25 12:01:32 +0100

    FIX migration scripts

--- account_credit_control/migrations/11.0.1.0.0/pre-migration.py
+++ account_credit_control/migrations/11.0.1.0.0/pre-migration.py
@@ -5,18 +5,31 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_tables(
-        env.cr,
-        [
-            ("credit_control_policy_credit_control_run_rel", "credit_run_policy_rel"),
-        ],
-    )
-    openupgrade.rename_columns(
-        env.cr,
-        {
-            "credit_run_policy_rel": [
-                ("credit_control_run_id", "run_id"),
-                ("credit_control_policy_id", "policy_id"),
-            ]
-        },
-    )
+    if openupgrade.table_exists(env.cr, "credit_run_policy_rel"):
+        # move data from credit_control_policy_credit_control_run_rel to
+        # credit_run_policy_rel
+        env.cr.execute(
+            """INSERT INTO credit_run_policy_rel
+               (run_id, policy_id)
+               SELECT credit_control_run_id, credit_control_policy_id
+               FROM credit_control_policy_credit_control_run_rel"""
+        )
+    else:
+        openupgrade.rename_tables(
+            env.cr,
+            [
+                (
+                    "credit_control_policy_credit_control_run_rel",
+                    "credit_run_policy_rel",
+                ),
+            ],
+        )
+        openupgrade.rename_columns(
+            env.cr,
+            {
+                "credit_run_policy_rel": [
+                    ("credit_control_run_id", "run_id"),
+                    ("credit_control_policy_id", "policy_id"),
+                ]
+            },
+        )
